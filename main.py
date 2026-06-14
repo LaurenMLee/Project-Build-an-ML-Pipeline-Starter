16bb1340e569da9ad6cbcf5c1c18ff59c7f9d7d5
LaurenMLee <lee.lauren.m@gmail.com>
2026-06-14 19:09:03 +0000

    lesson3inprogress

--- main.py
+++ main.py
@@ -49,17 +49,42 @@ def go(config: DictConfig):
                 },
             )
 
-        if "basic_cleaning" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+        if "basic_cleaning" in active_steps:                
+            _ = mlflow.run(
+                f"./src/basic_cleaning",
+                "main",
+                env_manager="conda",
+                parameters={
+                    "input_artifact": "sample.csv:latest",
+                    "output_artifact": "clean_sample.csv",
+                    "output_type": "clean_sample",
+                    "output_description": "Data with outliers removed",
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"],
+                },
+            )    
+            
+            
+            
 
         if "data_check" in active_steps:
             ##################
             # Implement here #
             ##################
-            pass
+            if "data_check" in active_steps:
+            _ = mlflow.run(
+                f"src/data_check",
+                "main",
+                env_manager="conda",
+                parameters={
+                    "csv": "clean_sample.csv:latest",
+                    "ref": "clean_sample.csv:reference",
+                    "kl_threshold": config["data_check"]["kl_threshold"],
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"],
+                },
+            )
+            
 
         if "data_split" in active_steps:
             ##################
